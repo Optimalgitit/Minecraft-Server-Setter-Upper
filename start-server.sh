42d58798328819774dc0ca267661f84135b4f76d
Optimalgitit <jggsecondary@gmail.com>
2019-11-16 22:11:49 +0400

    Fix no space after read

--- start-server.sh
+++ start-server.sh
@@ -6,6 +6,7 @@ echo "$PATH"
 echo "If you don't find java in these paths, you may have to add it."
 echo "Starting server, please wait..."
 echo "GUI or nah [y/N]: " && read -n 1 guiOrNot
+echo ""
 if [[ guiOrNot == y ]];
 then {
 	java -Xmx1024M -Xms1024M -jar server.jar
