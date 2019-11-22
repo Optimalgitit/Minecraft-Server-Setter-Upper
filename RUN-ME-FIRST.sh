11bfff92f433dc5473ac812a3be23bf2b125d103
Optimalgitit <jggsecondary@gmail.com>
2019-11-22 23:10:58 +0400

    something ain't working, still do not use

--- RUN-ME-FIRST.sh
+++ RUN-ME-FIRST.sh
@@ -10,10 +10,25 @@ $programName $programVer
 "
 echo "
 -------------------------
-Which version would uoi like to install?
+Which version would you like to install?
   - 1.14.4
   - 1.12.2
-  - 1.8.9
+  - 1.8.8
 -------------------------
 "
-read -p "> " serverVersion
+read serverVersion
+echo "Installing..."
+echo "Note: You will need to have wget installed."
+if [[ serverVersion == "1.14.4" ]]
+then
+    wget https://github.com/Optimalgitit/Minecraft-Server-Setter-Upper/raw/huge-update-beta/serverfiles/1.14.4.jar
+elif [[ serverVersion == "1.12.2" ]]
+then
+    wget https://github.com/Optimalgitit/Minecraft-Server-Setter-Upper/raw/huge-update-beta/serverfiles/1.12.2.jar
+elif [[ serverVersion == "1.8.8" ]]
+then
+    wget https://github.com/Optimalgitit/Minecraft-Server-Setter-Upper/raw/huge-update-beta/serverfiles/1.8.8.jar
+else
+    echo "Invalid option. Exiting..."
+    exit
+fi
