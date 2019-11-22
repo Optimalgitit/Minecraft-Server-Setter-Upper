cc9d87cf98cc6488fa1f115368d1d2749616b760
Optimalgitit <jggsecondary@gmail.com>
2019-11-22 22:15:45 +0400

    testing in progress. please do not use

---
+++ RUN-ME-FIRST.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+programName="Minecraft-Server-Setter-Upper"
+programVer=2.0
+
+echo "
+-------------------------
+Thank you for using
+$programName $programVer
+-------------------------
+"
+echo "
+-------------------------
+Which version would uoi like to install?
+  - 1.14.4
+  - 1.12.2
+  - 1.8.9
+-------------------------
+"
+read -p "> " serverVersion
